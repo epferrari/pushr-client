575d9af3fe0e3f310290a36c70c5fb5f91808b5a
Ethan Ferrari <ethan@ethanferrari.com>
2016-11-12 11:46:19 -0600

    updated intents

--- src/lib/intents.js
+++ src/lib/intents.js
@@ -1,6 +1,4 @@
-"use strict";
-
-export default {
+module.exports = {
   AUTH_REQ: 1,        // authentication request (client => server)
   AUTH_ACK: 2,        // authentication acknowledgement, credentials saved (server => client)
   AUTH_REJ: 3,        // authentication rejected, credentials were invalid (server => client)
@@ -20,7 +18,12 @@ export default {
   CLOSE_ACK: 14,      // connection close acknowledgement (server => client)
   CLOSE_ERR: 15,      // connection close error (server => client)
 
-  INVLD_INTENT: 16,   // invalid message intent (server => client)
+  INVLD_INT: 16,   // invalid message intent (server => client)
   INVLD_MSG: 17,      // invalid message shape (server => client)
-  PUSH: 18            // message pushed from server (server => client)
+  PUSH: 18,           // message pushed from server (server => client)
+
+  PUB_REQ: 19,        // message pushed from client (client => server)
+  PUB_ACK: 20,        // client-to-client push acknowledgement (server => client)
+  PUB_REJ: 21,        // client-to-client push rejection (server => client)
+  PUB_ERR: 22         // client-to-client push error (server => client)
 };
